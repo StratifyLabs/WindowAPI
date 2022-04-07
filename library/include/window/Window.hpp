1671d3d602ca7f008cd225d7d255e034f113b37c
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-04-07 17:08:28 -0600

    function to show/hide cursor (mouse)

--- library/include/window/Window.hpp
+++ library/include/window/Window.hpp
@@ -43,53 +43,50 @@ public:
   Window(
     const char *title,
     const Point &location,
-    const Size &size, Flags flags);
+    const Size &size,
+    Flags flags);
   ~Window();
 
-  u32 get_id() const {
-    return SDL_GetWindowID(m_native_pointer);
-  }
+  u32 get_id() const { return SDL_GetWindowID(m_native_pointer); }
 
   Size get_size() const {
     int w = 0;
     int h = 0;
     SDL_GetWindowSize(m_native_pointer, &w, &h);
-    return Size(w,h);
+    return Size(w, h);
   }
 
   Size get_minimum_size() const {
     int w = 0;
     int h = 0;
     SDL_GetWindowMinimumSize(m_native_pointer, &w, &h);
-    return Size(w,h);
+    return Size(w, h);
   }
 
   Size get_maximum_size() const {
     int w = 0;
     int h = 0;
     SDL_GetWindowMaximumSize(m_native_pointer, &w, &h);
-    return Size(w,h);
+    return Size(w, h);
   }
 
   Point get_position() const {
     int x = 0;
     int y = 0;
     SDL_GetWindowPosition(m_native_pointer, &x, &y);
-    return Point(x,y);
+    return Point(x, y);
   }
 
   PixelFormat get_pixel_format() const {
     return PixelFormat(SDL_GetWindowPixelFormat(m_native_pointer));
   }
 
-
-
-  Window& set_position(const Point & point){
+  Window &set_position(const Point &point) {
     SDL_SetWindowPosition(native_value(), point.x(), point.y());
     return *this;
   }
 
-  Window& set_size(const Size & size){
+  Window &set_size(const Size &size) {
     SDL_SetWindowSize(native_value(), size.width(), size.height());
     return *this;
   }
@@ -99,66 +96,68 @@ public:
     return Flags(SDL_GetWindowFlags(native_value()));
   }
 
-
-  Window& set_always_on_top(bool value = true){
+  Window &set_always_on_top(bool value = true) {
     SDL_SetWindowAlwaysOnTop(native_value(), SDL_bool(value));
     return *this;
   }
 
-  Window& set_bordered(bool value = true){
+  Window &set_bordered(bool value = true) {
     SDL_SetWindowBordered(native_value(), SDL_bool(value));
     return *this;
   }
 
-  Window& set_fullscreen(bool value = true){
+  Window &set_fullscreen(bool value = true) {
     SDL_SetWindowFullscreen(native_value(), SDL_bool(value));
     return *this;
   }
 
-  Window& set_resizeable(bool value = true){
+  Window &set_resizeable(bool value = true) {
     SDL_SetWindowResizable(native_value(), SDL_bool(value));
     return *this;
   }
 
-  Window& set_minimum_size(const Size & size){
+  Window &set_minimum_size(const Size &size) {
     SDL_SetWindowMinimumSize(native_value(), size.width(), size.height());
     return *this;
   }
 
-  Window& set_maximum_size(const Size & size){
+  Window &set_maximum_size(const Size &size) {
     SDL_SetWindowMaximumSize(native_value(), size.width(), size.height());
     return *this;
   }
 
-  static void enable_drop_file(){
+  static void enable_drop_file() {
     SDL_EventState(SDL_DROPBEGIN, SDL_ENABLE);
     SDL_EventState(SDL_DROPCOMPLETE, SDL_ENABLE);
     SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
     SDL_EventState(SDL_DROPTEXT, SDL_ENABLE);
   }
 
-  static void disable_drop_file(){
+  static void disable_drop_file() {
     SDL_EventState(SDL_DROPBEGIN, SDL_DISABLE);
     SDL_EventState(SDL_DROPCOMPLETE, SDL_DISABLE);
     SDL_EventState(SDL_DROPFILE, SDL_DISABLE);
-    SDL_EventState(SDL_DROPTEXT, SDL_DISABLE);  }
+    SDL_EventState(SDL_DROPTEXT, SDL_DISABLE);
+  }
+
+  static void show_cursor(bool value = true) {
+    SDL_ShowCursor(value ? SDL_ENABLE : SDL_DISABLE);
+  }
 
   static constexpr float opacity_transparent = 0.0f;
   static constexpr float opacity_opaque = 1.0f;
 
-  Window& set_opacity(float opacity){
+  Window &set_opacity(float opacity) {
     SDL_SetWindowOpacity(native_value(), opacity);
     return *this;
   }
-
-
 
 private:
   friend class Renderer;
   static bool m_is_initialized;
 };
 
-API_OR_NAMED_FLAGS_OPERATOR(Window,Flags)
+API_OR_NAMED_FLAGS_OPERATOR(Window, Flags)
 
 } // namespace window
 
